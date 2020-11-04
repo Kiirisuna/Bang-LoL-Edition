6c9251acf997655837057b177018ab88100d8021
Kiirisuna <31201780+Kiirisuna@users.noreply.github.com>
2020-11-05 04:46:02 +0800

    Revert "reverting"
    This reverts commit fc3ae33da29b20d8a3f942fba020a190b8937545.

---
+++ Assets/Character Cards/characterCard.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Card", menuName = "cards/Character")]
+public class characterCard : ScriptableObject {
+    public new string name;
+    public string description;
+
+    public Sprite artwork;
+
+    public int range;
+    public int health;
+    public int damage;
+    public int miss;
+    public int attack;
+
+
+}
